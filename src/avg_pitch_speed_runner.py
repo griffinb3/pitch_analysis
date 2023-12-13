60e46c91ce7ed577adc84c46785ac27e20a4e079
Griffinb3 <gbrown@westmont.edu>
2023-12-12 21:04:59 -0800

    final product

--- src/avg_pitch_speed_runner.py
+++ src/avg_pitch_speed_runner.py
@@ -1,24 +1,38 @@
-from avg_pitch_speed import *
+from src.avg_pitch_speed import PitchAnalysis
 
 
-def display_results(pitch_type):
-    avg_speed_obj = AveragePitchSpeed()
-    teams = group_by_teams()
-    team_speed_dict = avg_speed_obj.avg_pitch_speed(pitch_type)
+def display_results(pitch_type, analysis):
+    team_speed_dict = analysis.avg_pitch_speed(pitch_type)
 
     print(f"\nAverage Speed for {pitch_type} by Team (descending order):")
     for team, speed in team_speed_dict.items():
         print(f"{team}: {speed:.2f} mph")
 
 
-# Main runner
+def display_top_pitches(attribute1, attribute2, pitch_type, analysis):
+    top_pitches_data = analysis.top_pitches(attribute1, attribute2, pitch_type)
+
+    if top_pitches_data is not None:
+        print(f"\nPitches with the highest sum of standard deviations above the mean:")
+        print(top_pitches_data)
+
+
 if __name__ == "__main__":
+    FILE_PATH = r'C:\Users\griffyb3\cs128\2021_mlb_pitch_file_2.xlsx'
+    analysis = PitchAnalysis(FILE_PATH)
+
     # User input for the desired pitch type
-    user_pitch_input = input("Enter the pitch type (4-Seamer, Cutter, Sinker, Slider, Curveball, Changeup): ")
+    pitch_input = input("Enter the pitch type (4-Seamer, Cutter, Sinker, Slider, Curveball, Changeup): ")
 
     # Validate user input
     valid_pitch_types = ['4-Seamer', 'Cutter', 'Sinker', 'Slider', 'Curveball', 'Changeup']
-    if user_pitch_input not in valid_pitch_types:
+    if pitch_input not in valid_pitch_types:
         print("Invalid pitch type. Please enter a valid pitch type.")
     else:
-        display_results(user_pitch_input)
+        display_results(pitch_input, analysis)
+
+    attribute_input1 = input("Enter the first attribute (avg_speed, Spin Rate): ")
+    attribute_input2 = input("Enter the second attribute (avg_speed, Spin Rate):  ")
+
+    # Calculate sum of standard deviations above the mean and display top players
+    display_top_pitches(attribute_input1, attribute_input2, pitch_input, analysis)
