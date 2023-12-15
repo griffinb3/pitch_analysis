0bd952e9e036eb92e3344c0cd082b2270f311534
griffinb3 <gbrown@westmont.edu>
2023-12-14 17:25:22 -0800

    commit

--- src/pitch_analysis/avg_pitch_speed.py
+++ src/pitch_analysis/avg_pitch_speed.py
@@ -2,22 +2,6 @@
 
 import pandas as pd
 
-# path = os.path.join(os.getcwd(), 'assignment-5-gcp-a25d09364c28.json')
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
-
-# create storage client variable
-# storage_client = storage.Client(path)
-
-# bucket = storage_client.get_bucket('cs128_bucket')
-
-# want to read the contents of this bucket
-# file = [filename.name for filename in list(bucket.list_blobs(prefix=''))]
-# print("Files present here are: ", file)
-
-# get our file
-# blob = bucket.blob(blob_name='2021_mlb_pitch_file_2.xlsx').download_as_string()
-# with open('local_dataset.xlsx', 'wb') as f:
-    # f.write(blob)
 
 
 class PitchAnalysis:
@@ -40,44 +24,114 @@ class PitchAnalysis:
         team_speed_dict = dict(sorted(zip(self.team_list, pitch_list), key=lambda x: x[1], reverse=True))
         return team_speed_dict
 
-    """def avg_pitch_speed(self, pitch_type):
-        team_speed_list = []
-        # For each team in the league, calculate average speed for the pitch given
-        for name in self.team_list:
-            team_speed_list.append({
-                'team_name': name,
-                'avg_speed': self.df[(self.df['team_name'] == name) & (self.df['pitch_type_name'] == pitch_type)][
-                    'avg_speed'].mean()
-            })
+    # Load the data from the Excel file
 
-        # Sort the list of dictionaries based on average speed in descending order
-        team_speed_list = sorted(team_speed_list, key=lambda x: x['avg_speed'], reverse=True)
 
-        return team_speed_list"""
+# Group data by team and pitch type, calculate average pitch velocity and spin rate
+# This was the initial code that I created. I asked ChatGPT to make it generic
+"""def group_by_teams():
+    for name in df['team_name'].unique():
+        if name not in team_list:
+            team_list.append(name)
+    return team_list
 
-    def top_pitches(self, attribute1, attribute2, pitch_type):
-        for attribute in [attribute1, attribute2]:
-            if attribute not in self.df.columns:
-                print(f"Invalid attribute: {attribute}. Please enter a valid attribute.")
-                return
 
-        # Calculate Mean and Standard Deviation for each attribute
-        mean_value1 = self.df[attribute1].mean()
-        std_dev1 = self.df[attribute1].std()
-        mean_value2 = self.df[attribute2].mean()
-        std_dev2 = self.df[attribute2].std()
+class AveragePitchSpeed:
+    @staticmethod
+    def avg_fb_speed():
+        # currently have the data sorted by team right now
+        # want to find a way to get all the pitches that are fastballs from that team
+        speed_df_fb = df['avg_speed']
+        fb_list = []
+        for name in team_list:
+            if df['pitch_type_name'] == '4-seamer':
+                fb_list.append(speed_df_fb)
+        avg_fb_speed = sum(fb_list) / len(fb_list)
+        team_speed_fb = {name: avg_fb_speed}
+        return team_speed_fb
 
-        # Filter pitches by pitch given previously
-        pitch_df = self.df[self.df['pitch_type_name'] == pitch_type].copy()
+    @staticmethod
+    def avg_ch_speed():
+        speed_df_ch = df['avg_speed']
+        ch_list = []
+        for name in team_list:
+            if df['pitch_type_name'] == 'Changeup':
+                ch_list.append(speed_df_ch)
+        avg_ch_speed = sum(ch_list) / len(ch_list)
+        team_speed_ch = {name: avg_ch_speed}
+        return team_speed_ch
 
-        # Calculate standard deviations above the mean for each pitch
-        pitch_df['StdDevs1'] = (pitch_df[attribute1] - mean_value1) / std_dev1
-        pitch_df['StdDevs2'] = (pitch_df[attribute2] - mean_value2) / std_dev2
+    @staticmethod
+    def avg_cb_speed():
+        speed_df_cb = df['avg_speed']
+        cb_list = []
+        for name in team_list:
+            if df['pitch_type_name'] == 'Curveball':
+                cb_list.append(speed_df_cb)
+        avg_cb_speed = sum(cb_list) / len(cb_list)
+        team_speed_cb = {name: avg_cb_speed}
+        return team_speed_cb
 
-        # Sum of standard deviations above the mean for both attributes
-        pitch_df['SumStdDevs'] = pitch_df['StdDevs1'] + pitch_df['StdDevs2']
+    @staticmethod
+    def avg_cut_speed():
+        speed_df_cut = df['avg_speed']
+        cut_list = []
+        for name in team_list:
+            if df['pitch_type_name'] == 'Cutter':
+                cut_list.append(speed_df_cut)
+        avg_cut_speed = sum(cut_list) / len(cut_list)
+        team_speed_cut = {name: avg_cut_speed}
+        return team_speed_cut
 
-        # Find players with the highest values
-        top_players = pitch_df.nlargest(20, 'SumStdDevs')
+    @staticmethod
+    def avg_sink_speed():
+        speed_df_sink = df['avg_speed']
+        sink_list = []
+        for name in team_list:
+            if df['pitch_type_name'] == 'Sinker':
+                sink_list.append(speed_df_sink)
+        avg_sink_speed = sum(sink_list) / len(sink_list)
+        team_speed_sink = {name: avg_sink_speed}
+        return team_speed_sink
 
-        return top_players[[' first_name', 'last_name', attribute1, attribute2, 'SumStdDevs']]
+    @staticmethod
+    def avg_sld_speed():
+        speed_df_sld = df['avg_speed']
+        sld_list = []
+        for name in team_list:
+            if df['pitch_type_name'] == 'Slider':
+                sld_list.append(speed_df_sld)
+        avg_sld_speed = sum(sld_list) / len(sld_list)
+        team_speed_sld = {name: avg_sld_speed}
+        return team_speed_sld 
+        
+"""
+
+
+
+def top_pitches(self, attribute1, attribute2, pitch_type):
+    for attribute in [attribute1, attribute2]:
+        if attribute not in self.df.columns:
+            print(f"Invalid attribute: {attribute}. Please enter a valid attribute.")
+            return
+
+    # Calculate Mean and Standard Deviation for each attribute
+    mean_value1 = self.df[attribute1].mean()
+    std_dev1 = self.df[attribute1].std()
+    mean_value2 = self.df[attribute2].mean()
+    std_dev2 = self.df[attribute2].std()
+
+    # Filter pitches by pitch given previously
+    pitch_df = self.df[self.df['pitch_type_name'] == pitch_type].copy()
+
+    # Calculate standard deviations above the mean for each pitch
+    pitch_df['StdDevs1'] = (pitch_df[attribute1] - mean_value1) / std_dev1
+    pitch_df['StdDevs2'] = (pitch_df[attribute2] - mean_value2) / std_dev2
+
+    # Sum of standard deviations above the mean for both attributes
+    pitch_df['SumStdDevs'] = pitch_df['StdDevs1'] + pitch_df['StdDevs2']
+
+    # Find players with the highest values
+    top_players = pitch_df.nlargest(20, 'SumStdDevs')
+
+    return top_players[[' first_name', 'last_name', attribute1, attribute2, 'SumStdDevs']]
